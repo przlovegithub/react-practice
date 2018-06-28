3966d82beefc209644a7316e72db6f68ae4df884
Ts`ui Cheng-chih <prz1991412@126.com>
2018-06-28 11:00:33 +0800

    delete baseUrl

--- src/service/env.js
+++ src/service/env.js
@@ -6,11 +6,11 @@
  */
 
 let baseUrl = '';
-
+console.log(process.env.HOST, process.env)
 if (process.env.NODE_ENV === 'development') {
     console.log("this is development")
         // baseUrl = 'http://app.mazdamall.cn/';
-    baseUrl = 'http://192.168.3.56:9999';
+        // baseUrl = 'http://192.168.3.56:9999';
 } else if (process.env.NODE_ENV === 'production') {
     console.log("this is production")
         // baseUrl = 'http://app.mazdamall.cn/';
