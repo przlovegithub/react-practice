4f39a36cebf2e7f568aacbf24374f422b7845be2
przlovegithub <prz1991412@126.com>
2018-08-27 18:08:48 +0800

    [hash:8] changes to timeStamp

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -27,7 +27,9 @@ const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 const publicUrl = publicPath.slice(0, -1);
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
-// alias
+// timeStamp
+const timeStamp = new Date().toLocaleString().replace(/[-:\s]+/g, '')
+    // alias
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
@@ -38,7 +40,8 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+// const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = `static/css/[name].${timeStamp}.css`;
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -64,8 +67,11 @@ module.exports = {
         // Generated JS file names (with nested folders).
         // There will be one main bundle, and one file per asynchronous chunk.
         // We don't currently advertise code splitting but Webpack supports it.
-        filename: 'static/js/[name].[chunkhash:8].js',
-        chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+
+        // filename: 'static/js/[name].[chunkhash:8].js',
+        // chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+        filename: `static/js/[name].${timeStamp}.js`,
+        chunkFilename: `static/js/[name].${timeStamp}.chunk.js`,
         // We inferred the "public path" (such as / or /my-project) from homepage.
         publicPath: publicPath,
         // Point sourcemap entries to original disk location (format as URL on Windows)
@@ -140,7 +146,8 @@ module.exports = {
                         loader: require.resolve('url-loader'),
                         options: {
                             limit: 10000,
-                            name: 'static/media/[name].[hash:8].[ext]',
+                            // name: 'static/media/[name].[hash:8].[ext]',
+                            name: `static/media/[name].${timeStamp}.[ext]`,
                         },
                     },
                     // Process JS with Babel.
@@ -228,7 +235,8 @@ module.exports = {
                         // by webpacks internal loaders.
                         exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
                         options: {
-                            name: 'static/media/[name].[hash:8].[ext]',
+                            // name: 'static/media/[name].[hash:8].[ext]',
+                            name: `static/media/[name].${timeStamp}.[ext]`,
                         },
                     },
                     // ** STOP ** Are you adding a new loader?
